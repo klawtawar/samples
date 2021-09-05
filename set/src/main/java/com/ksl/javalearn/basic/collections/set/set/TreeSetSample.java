ee140ee83cf4f74e7d58b7b1bb23dd2ca9b15e76
kunal <klawtawar@gmail.com>
2021-09-04 19:12:43 -0500

    updates

---
+++ set/src/main/java/com/ksl/javalearn/basic/collections/set/set/TreeSetSample.java
@@ -0,0 +1,12 @@
+package com.ksl.javalearn.basic.collections.set.set;
+
+public class TreeSetSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello set");
+
+	}
+
+}
